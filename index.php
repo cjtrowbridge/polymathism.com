d977bf04dfcae86b8327c2ba4088c80108c9b462
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-02 17:17:20 -0800

    Update index.php

--- index.php
+++ index.php
@@ -25,17 +25,17 @@ $URL=rtrim(ltrim($_SERVER['REQUEST_URI'],'/'),'/');
           <!-- Nav tabs -->
           <ul class="nav nav-tabs" role="tablist">
             <li class="nav-item">
-              <a class="nav-link actvie" data-toggle="tab" href="/" role="tab" onclick="$('#polymathismTab').tab('show');">Polymathism</a>
+              <a class="nav-link actvie" data-toggle="tab" href="#polymathism" role="tab" >Polymathism</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" data-toggle="tab" href="/news" role="tab" onclick="$('#newsTab').tab('show');">News</a>
+              <a class="nav-link" data-toggle="tab" href="#news" role="tab">News</a>
             </li>
           </ul>
 
           <!-- Tab panes -->
           <div class="tab-content">
-            <div class="tab-pane active" id="polymathismTab" role="tabpanel">polymathism content here</div>
-            <div class="tab-pane" id="newsTab" role="tabpanel">news content here</div>
+            <div class="tab-pane active" id="polymathism" role="tabpanel">polymathism content here</div>
+            <div class="tab-pane" id="news" role="tabpanel">news content here</div>
           </div>
           
         </div>

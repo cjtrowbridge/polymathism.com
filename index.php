4717cfa488a2f675a3aba7e5663d20c65afd69fe
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-02 17:29:26 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
-
-$URL=rtrim(ltrim($_SERVER['REQUEST_URI'],'/'),'/');
+if(isset($_GET['feed'])){
+ switch($_GET['feed']){
+   case 'npr-podcast':
+     FetchFeed('https://www.npr.org/rss/podcast.php?id=500005');
+   case 'cbc-podcast':
+     FetchFeed('http://www.cbc.ca/podcasting/includes/hourlynews.xml');
+   case 'dw-podcast':
+     FetchFeed('http://rss.dw.com/xml/podcast_news');
+   default:
+     die('Unknown Feed');
+ }
+}
+function FetchFeed($url){
+  header("Content-type: text/xml");
+  echo file_get_contents($url);
+  exit;
+}
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -8,7 +23,13 @@ $URL=rtrim(ltrim($_SERVER['REQUEST_URI'],'/'),'/');
     <title>Polymathism</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content=""/>
+    <meta name="description" content="">
+    <meta property="og:title" content="Polymathism">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="" />
+    <meta property="og:url" content="https://polymathism.com">
+    <meta property="og:image" content="">
+    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <style>
       body{
@@ -34,8 +55,30 @@ $URL=rtrim(ltrim($_SERVER['REQUEST_URI'],'/'),'/');
 
           <!-- Tab panes -->
           <div class="tab-content">
+            
             <div class="tab-pane active" id="polymathism" role="tabpanel">polymathism content here</div>
-            <div class="tab-pane" id="news" role="tabpanel">news content here</div>
+            
+            <div class="tab-pane" id="news" role="tabpanel">
+              <div class="podcastContainer" data-feed="/?feed=npr-podcast">
+                <img src="https://media.npr.org/images/podcasts/2013/primary/hourly_news_summary-c464279737c989a5fbf3049bc229152af3c36b9d.png?s=1400">
+                <h4>NPR: Hourly World News</h4>
+                <div class="player"></div>
+                <div class="clearer"></div>
+              </div>
+              <div class="podcastContainer" data-feed="/?feed=cbc-podcast">
+                <img src="http://www.cbc.ca/podcasting/images/promo-hourlies.jpg">
+                <h4>CBC: Hourly World News</h4>
+                <div class="player"></div>
+                <div class="clearer"></div>
+              </div>
+              <div class="podcastContainer" data-feed="/?feed=dw-podcast">
+                <img src="http://www.dw.com/image/2135752_7.jpg">
+                <h4>Deutsche Welle: Hourly World News</h4>
+                <div class="player"></div>
+                <div class="clearer"></div>
+              </div>
+            </div>
+            
           </div>
           
         </div>
@@ -46,6 +89,7 @@ $URL=rtrim(ltrim($_SERVER['REQUEST_URI'],'/'),'/');
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     
+    <script src="/js/podcastLoader.js"></script>
     <script>
       $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
         e.target // newly activated tab

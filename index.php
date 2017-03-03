399502ef00239eb54ddcad1891e4a6dcf3af3dd9
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-02 16:45:57 -0800

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content=""/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <style>
+      body{
+        padding-top: 2rem;
+      }
+    </style>
   </head>
   <body>
     
